6cd5ee9630f249e189a42ca6d7e9998ebcbcfc74
Miseong Kim <miseongk@naver.com>
2024-07-12 14:56:37 +0900

    feat: 로또 번호 매칭, 보너스 번호 매칭 기능 구현

--- src/main/kotlin/domain/Lotto.kt
+++ src/main/kotlin/domain/Lotto.kt
@@ -9,6 +9,14 @@ class Lotto(var numbers: List<Int>) {
         numbers = numbers.sorted()
     }
 
+    fun matchBall(winningLotto: Lotto): Int {
+        return winningLotto.numbers.count { it in numbers }
+    }
+
+    fun isBonusBallMatched(bonusNumber: Int): Boolean {
+        return bonusNumber in numbers
+    }
+
     companion object {
         private const val LOTTO_NUMBER_SIZE = 6
     }
--- src/test/kotlin/domain/LottoTest.kt
+++ src/test/kotlin/domain/LottoTest.kt
@@ -3,6 +3,8 @@ package domain
 import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class LottoTest : StringSpec({
@@ -27,6 +29,30 @@ class LottoTest : StringSpec({
             lotto.numbers[3] shouldBe 4
             lotto.numbers[4] shouldBe 5
             lotto.numbers[5] shouldBe 6
+        }
+    }
+
+    "로또 번호를 매칭하여 매칭되는 볼의 개수를 반환한다" {
+        val lotto = Lotto(listOf(4, 5, 6, 7, 8, 9))
+
+        forAll(
+            row(Lotto(listOf(11, 12, 13, 14, 15, 16)), 0),
+            row(Lotto(listOf(1, 2, 3, 4, 5, 6)), 3),
+        ) { winningLotto, expected ->
+            val result = lotto.matchBall(winningLotto)
+            result shouldBe expected
+        }
+    }
+
+    "보너스 볼과 매칭되는 볼이 있으면 true, 없으면 false를 반환한다" {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+
+        forAll(
+            row(1, true),
+            row(10, false),
+        ) { bonusBallNumber, expected ->
+            val result = lotto.isBonusBallMatched(bonusBallNumber)
+            result shouldBe expected
         }
     }
 })
